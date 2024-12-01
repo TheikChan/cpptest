51766e99b991175cfd8ab2a48fac90eedc1ea32c
Theik Chan <theikchan.tc@gmail.com>
2024-12-01 22:01:51 +0700

    feat: switch statement, loop and struct

--- cpptest.cpp
+++ cpptest.cpp
@@ -19,6 +19,26 @@ bool isValidName(string name)
     return name.length() > 0;
 }
 
+// named struct
+struct
+{
+    int myNum;
+    string myString;
+} myStructure, myStruct2, myStruct3; // Multiple structure variables separated with commas
+
+struct myDataType
+{
+    int myNum;
+    string myString;
+};
+
+struct car
+{
+    string brand;
+    string model;
+    int year;
+};
+
 int main()
 {
 
@@ -94,8 +114,78 @@ int main()
         cout << "Not old enough to vote.";
     }
 
+    cout << (myAge >= votingAge) ? "Old enough to vote!" : "Not old enough to vote."; // ternary operator
+
+    // switch statement
+    int day = 4;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    default:
+        cout << "Looking forward to the Weekend";
+    }
+
+    // while loop
+    int i = 0;
+    while (i < 5)
+    {
+        cout << i << "\n";
+        i++;
+    }
+
+    // do while loop execute at leat one before checking the condition
+    int j = 0;
+    do
+    {
+        cout << j << "\n";
+        j++;
+    } while (j < 5);
+
+    // for loop
+    for (int i = 0; i <= 50; i += 10)
+    {
+        if (i == 20)
+        {
+            continue; // skip execution
+        }
+        if (i == 40)
+        {
+            break; // exit loop
+        }
+        cout << i << "\n";
+    }
     // msg array
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"}; // vector resizable array
+    string message[] = {};
+
+    string cars[3] = {"Volvo", "BMW", "Ford"}; // fixed size array
+
+    int msgSize = sizeof(msg); // size of msg array
+
+    // multi dimensional array
+    string letters[2][4] = {
+        {"A", "B", "C", "D"},
+        {"E", "F", "G", "H"}};
 
     // insert new item
     msg.push_back("theik");
@@ -197,4 +287,20 @@ int main()
     {
         cout << "invalid name" << endl;
     }
+
+    // Assign values to members of myStructure
+    myStructure.myNum = 1;
+    myStructure.myString = "Hello World!";
+
+    // Print members of myStructure
+    cout << myStructure.myNum << "\n";
+    cout << myStructure.myString << "\n";
+
+    myDataType myVar;
+
+    car myCar;
+
+    myCar.brand = "Toyata";
+    myCar.model = "Hilux";
+    myCar.year = 2010;
 }
